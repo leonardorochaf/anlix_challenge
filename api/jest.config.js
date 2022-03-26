0fd9855d5613d9446133e119cb05e07443324b38
Leonardo Rocha <leonardo.rocha0803@gmail.com>
2022-03-26 14:00:48 -0300

    chore: add jest and husky configuration

---
+++ api/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  roots: [
+    '<rootDir>/tests',
+    '<rootDir>/src'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  },
+  clearMocks: true
+}
