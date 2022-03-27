15cffaf56f47ba8bbeaa44d0e6180ca3a5406e73
Leonardo Rocha <leonardo.rocha0803@gmail.com>
2022-03-27 14:01:59 -0300

    chore: add server file to jest coverage ignore

--- api/jest.config.js
+++ api/jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
     '!<rootDir>/src/**/index.ts',
     '!<rootDir>/src/config/**',
     '!<rootDir>/src/database/**',
-    '!<rootDir>/src/validation/custom-validation-decorators/**'
+    '!<rootDir>/src/validation/custom-validation-decorators/**',
+    '!<rootDir>/src/server.ts'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
