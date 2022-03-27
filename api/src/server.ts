8ba1b2bfe77839ae1b809b9e52f79b73f74ce53c
Leonardo Rocha <leonardo.rocha0803@gmail.com>
2022-03-27 13:50:55 -0300

    feat: create dev server

---
+++ api/src/server.ts
@@ -0,0 +1,15 @@
+
+import './config/module-alias'
+
+import { env } from '@/config/env'
+import { PgConnection } from '@/database'
+
+PgConnection.getInstance().connect()
+  .then(async () => {
+    console.log('Connected to db')
+    const { app } = await import('./app')
+    app.listen(env.APP.PORT, () => console.log(`Server running on port ${env.APP.PORT}`))
+  })
+  .catch(e => {
+    console.error(e)
+  })
