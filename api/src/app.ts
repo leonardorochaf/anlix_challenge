630ded78519094629d56930238ce150ef734df29
Leonardo Rocha <leonardo.rocha0803@gmail.com>
2022-03-28 18:39:02 -0300

    feat: add cors package

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 import { initRoutes } from './routes/router'
 
@@ -6,6 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 initRoutes(app)
 
